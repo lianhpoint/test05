da4de9d297012550b1d86fd03422abd86ed4b34b
liang <1187468864@qq.com>
2022-06-18 20:40:03 +0800

    push_test

--- src/main/java/com/liang/test01.java
+++ src/main/java/com/liang/test01.java
@@ -4,5 +4,6 @@ public class test01 {
     public static void main(String[] args) {
         System.out.println("test01");
         System.out.println("pull");
+        System.out.println("push");
     }
 }
